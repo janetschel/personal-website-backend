56677c192ddb95603d560458dccccf1624a4afcf
thvndr <90684712+thvndr@users.noreply.github.com>
2022-02-15 17:39:32 +0100

    Update App.java

--- src/main/java/de/janetschel/personal/website/backend/App.java
+++ src/main/java/de/janetschel/personal/website/backend/App.java
@@ -2,7 +2,7 @@ package de.janetschel.personal.website.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// ICH MAG PORNOS
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
